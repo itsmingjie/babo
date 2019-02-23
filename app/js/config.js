2ea43108f9b0765eb80aa0cfa5e9734b3ca8e4f7
Mingjie Jiang <jiang@mingjie.info>
2019-02-23 09:50:29 -0500

    Add Hotjar

--- app/js/config.js
+++ app/js/config.js
@@ -8,3 +8,20 @@ const MANTRA = [
     "great",
     "always the best"
 ]
+
+/* Hotjar Tracking Code for https://babo.mingjie.codes */
+
+(function (h, o, t, j, a, r) {
+    h.hj = h.hj || function () {
+        (h.hj.q = h.hj.q || []).push(arguments)
+    };
+    h._hjSettings = {
+        hjid: 1211284,
+        hjsv: 6
+    };
+    a = o.getElementsByTagName('head')[0];
+    r = o.createElement('script');
+    r.async = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
