328ab1e5933fdebd349cd6f3f0a9ef4cd5915438
Mingjie Jiang <jiang@mingjie.info>
2019-03-23 22:34:05 -0400

    Design overhaul

--- app/js/config.js
+++ app/js/config.js
@@ -1,27 +1,29 @@
 const ROOTDOMAIN = "localhost:3000";
 const MANTRA = [
-    "fabulous",
-    "awesome",
-    "fantastic",
-    "doing great",
-    "amazing",
-    "great",
-    "always the best"
+  "fabulous",
+  "awesome",
+  "fantastic",
+  "doing great",
+  "amazing",
+  "great",
+  "always the best"
 ];
 
 /* Hotjar Tracking Code for https://babo.mingjie.codes */
 
-(function (h, o, t, j, a, r) {
-    h.hj = h.hj || function () {
-        (h.hj.q = h.hj.q || []).push(arguments)
+(function(h, o, t, j, a, r) {
+  h.hj =
+    h.hj ||
+    function() {
+      (h.hj.q = h.hj.q || []).push(arguments);
     };
-    h._hjSettings = {
-        hjid: 1211284,
-        hjsv: 6
-    };
-    a = o.getElementsByTagName('head')[0];
-    r = o.createElement('script');
-    r.async = 1;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-    a.appendChild(r);
-})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+  h._hjSettings = {
+    hjid: 1211284,
+    hjsv: 6
+  };
+  a = o.getElementsByTagName("head")[0];
+  r = o.createElement("script");
+  r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
