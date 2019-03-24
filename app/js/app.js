95c172245978b6a3fc723c05ea10056ce2455a30
Mingjie Jiang <jiang@mingjie.info>
2019-03-24 11:06:18 -0400

    Design overhaul 3.0

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,55 @@ Date.prototype.toDateInputValue = function() {
   return local.toJSON().slice(0, 10);
 };
 
+// PWA Helper
+function addToHomeScreen() {
+  let a2hsBtn = document.querySelector(".ad2hs-prompt"); // hide our user interface that shows our A2HS button
+  a2hsBtn.style.display = "none"; // Show the prompt
+  deferredPrompt.prompt(); // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then(function(choiceResult) {
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the A2HS prompt");
+    } else {
+      console.log("User dismissed the A2HS prompt");
+    }
+    deferredPrompt = null;
+  });
+}
+function showAddToHomeScreen() {
+  let a2hsBtn = document.querySelector(".ad2hs-prompt");
+  a2hsBtn.style.display = "block";
+  a2hsBtn.addEventListener("click", addToHomeScreen);
+}
+let deferredPrompt;
+window.addEventListener("beforeinstallprompt", function(e) {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  showAddToHomeScreen();
+});
+
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test(userAgent);
+};
+// Detects if device is in standalone mode
+const isInStandaloneMode = () =>
+  "standalone" in window.navigator && window.navigator.standalone;
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  showIosInstall();
+}
+
+function showIosInstall() {
+  let iosPrompt = document.querySelector(".ios-prompt");
+  iosPrompt.style.display = "block";
+  iosPrompt.addEventListener("click", () => {
+    iosPrompt.style.display = "none";
+  });
+}
+
 function loggedInActions(dn) {
   user = firebase.auth().currentUser;
 
@@ -72,7 +121,7 @@ function loadLogs() {
                 timestamp: moment
                   .unix(d.timestamp.seconds)
                   .format("h:mm:ss A, MMMM Do, YYYY"),
-                price: d.price,
+                price: parseFloat(d.price).toFixed(2),
                 vendor: d.vendor,
                 size: d.size,
                 flavor: d.flavor
